979607b9a1c658ebc9d235b7501563edc51697cd
Srikanteswararao Talluri <talluri.work@gmail.com>
2018-02-16 00:34:42 +0530

    test commit

--- src/main/java/NewHelloWorld.java
+++ src/main/java/NewHelloWorld.java
@@ -1,5 +1,11 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class NewHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.setProperty("webdriver.chrome.driver", "/Users/stalluri/Downloads/chromedriver");
+        ChromeDriver cd = new ChromeDriver();
+        cd.get("https://google.com");
+
     }
 }
